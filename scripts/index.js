62ab79c57725930585c9754f1e1f2265bee440e1
H0rn0chse <h0rn0chse@gmx.de>
2019-10-13 18:37:53 +0200

    Added Sections

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,25 @@
 window.onload = () => {
-	console.log("loaded")
+	document.getElementById("defaultOpen").click();
+	Manager.init(true);
 };
 
-window.writeLocalStorage = () => {
-	window.localStorage.setItem('name', 'Obaseki Nosa');
-};
+function openTab(evt, tabName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
 
-window.readLocalStorage = () => {
-	window.localStorage.getItem('name');
-};
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+}

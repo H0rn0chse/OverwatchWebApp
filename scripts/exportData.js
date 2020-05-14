b73526f4e425608048846323caaf2fa546ef77d5
H0rn0chse <h0rn0chse@gmx.de>
2020-05-14 21:46:16 +0200

    Added export functionality

---
+++ scripts/exportData.js
@@ -0,0 +1,12 @@
+function exportEntries () {
+	const text = JSON.stringify(items);
+	download(text, 'entries.txt', 'text/plain');
+}
+
+function download(content, fileName, contentType) {
+    const a = document.createElement("a");
+    const file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}

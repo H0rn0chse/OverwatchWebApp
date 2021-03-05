be5f0e201a401d92f74cd8bf4a85b7a8d2c25437
H0rn0chse <h0rn0chse@gmx.de>
2021-03-05 23:40:25 +0100

    Added acknowledgements and licenses

--- scripts/handler.ts
+++ scripts/handler.ts
@@ -8,50 +8,54 @@ import { exportEntries } from "./exportData.js";
 
 declare const window: any;
 
-export function init () {
-    window.addEventListener("load", () => {
-        initIgnoreDirtyState(document.querySelector("#cbxDirtyState"));
-        loadFromLocalStorage();
-        initCharts();
-        rebuildTable()
-        setDirtyState(false);
-        window.dispatchEvent(new Event("updateAll"));
-    });
+window.addEventListener("load", () => {
+    initIgnoreDirtyState(document.querySelector("#cbxDirtyState"));
+    loadFromLocalStorage();
+    initCharts();
+    rebuildTable()
+    setDirtyState(false);
+    window.dispatchEvent(new Event("updateAll"));
+});
 
-    window.addEventListener("beforeunload", (event) => {
-        if (getDirtyState()) {
-            var message = 'You may export the latest changes';
-            event.returnValue = message;
-            return message;
-        }
-    });
+window.addEventListener("beforeunload", (event) => {
+    if (getDirtyState()) {
+        var message = 'You may export the latest changes';
+        event.returnValue = message;
+        return message;
+    }
+});
 
-    window.addEventListener("updateAll", () => {
-        updateSeasonSelect();
-        updateCharts();
-        updateStats();
-        updateInfo();
+window.addEventListener("updateAll", () => {
+    updateSeasonSelect();
+    updateCharts();
+    updateStats();
+    updateInfo();
+})
+
+window.updateStats = updateStats;
+window.updateSeasonStats = () => {
+    updateCharts();
+};
+window.addRow = addRow;
+window.exportEntries = exportEntries;
+window.importEntries = importEntries;
+window.setProgressChart = (event) => {
+    // Reset Charts
+    const progressCharts = ["ctxProgress1", "ctxProgress2"];
+    progressCharts.forEach(value => {
+        const element = document.getElementById(value);
+        element.style.display = "none";
     })
 
-    window.updateStats = updateStats;
-    window.updateSeasonStats = () => {
-        updateCharts();
-    };
-    window.addRow = addRow;
-    window.exportEntries = exportEntries;
-    window.importEntries = importEntries;
-    window.setProgressChart = (event) => {
-        // Reset Charts
-        const progressCharts = ["ctxProgress1", "ctxProgress2"];
-        progressCharts.forEach(value => {
-            const element = document.getElementById(value);
-            element.style.display = "none";
-        })
-
-        // Show correct chart
-        const select = <HTMLSelectElement>document.getElementById("progressSelect");
-        const value = select.value;
-        const element = document.getElementById(value);
-        element.style.display = "";
-    }
+    // Show correct chart
+    const select = <HTMLSelectElement>document.getElementById("progressSelect");
+    const value = select.value;
+    const element = document.getElementById(value);
+    element.style.display = "";
 }
+
+const acknowledgements = document.querySelector("#acknowledgements");
+acknowledgements.innerHTML = globalThis.feather.icons["award"].toSvg({ color: "#e2b007" });
+acknowledgements.addEventListener("click", evt => {
+    window.open("./acknowledgements/third-party-licenses.html", '_blank')
+}, { passive: true });

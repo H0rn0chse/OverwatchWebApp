cdd44bd11a7873ef37cf9a75c25f8153e5e192f7
H0rn0chse <h0rn0chse@gmx.de>
2022-02-26 00:21:15 +0100

    Updating workflow and adding anonymized statistics

--- server.js
+++ server.js
@@ -1,27 +1,9 @@
 import { startServer } from "@h0rn0chse/socket-server";
+import fs from "fs";
+
+const json = fs.readFileSync("./gh-pages-dependencies.json")
+const dependencies = JSON.parse(json);
 
 startServer({
-    publicPaths: [[
-        "/client", "/"
-    ], [
-        "/node_modules/chart.js/dist/", "/libs/chart.js"
-    ], [
-        "/node_modules/lodash/", "/libs/lodash"
-    ], [
-        "/node_modules/bootstrap/dist/", "/libs/bootstrap"
-    ], [
-        "/node_modules/bootstrap-vue/dist/", "/libs/bootstrap-vue"
-    ], [
-        "/node_modules/vue/dist/", "/libs/vue"
-    ], [
-        "/node_modules/vuex/dist/", "/libs/vuex"
-    ], [
-        "/node_modules/vue-chartjs/dist/", "/libs/vue-chartjs"
-    ], [
-        "/node_modules/lottie-web/build/", "/libs/lottie-web"
-    ], [
-        "/node_modules/@h0rn0chse/dark-mode-toggle/dist/", "/libs/dark-mode-toggle"
-    ], [
-        "/node_modules/wc-github-corners/dist", "/libs/wc-github-corners"
-    ]]
+    publicPaths: dependencies
 });
